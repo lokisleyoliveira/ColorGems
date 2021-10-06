c2e3f12ce82969a3cd302d7ba9297ce18b6d5dca
Lokisley Oliveira <lokisley@hotmail.com>
2021-10-05 23:10:58 -0300

    :hammer: Add enemy chasing script

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public int id;
+    public Transform goal;
+    public float moveSpeed = 1.5f;
+
+    private bool triggered = false;
+    private Vector2 movement;
+    private Rigidbody2D rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidBody = this.GetComponent<Rigidbody2D>();
         GameEvents.current.onGemColisionEnter += OnGemCollected;
         GameEvents.current.onGemDrop += onGemDropped;
+    }
+
+    private void Update()
+    {
+        Vector2 direction = goal.position - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate()
+    {
+        if (triggered)
+        {
+            moveEnemy(movement);
+        }
     }
 
     private void OnGemCollected(int id)
     {
         if (id == this.id)
         {
-            Destroy(this.gameObject);
+            triggered = true;
         }
     }
 
@@ -25,7 +48,12 @@ public class EnemyController : MonoBehaviour
     {
         if (id == this.id)
         {
-
+            triggered = false;
         }
+    }
+
+    private void moveEnemy(Vector2 direction)
+    {
+        rigidBody.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.fixedDeltaTime));
     }
 }
---
+++ Assets/Scripts/EnemySeparation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySeparation : MonoBehaviour
+{
+    GameObject[] enemies;
+    public float spaceBetween = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (GameObject go in enemies)
+        {
+            if (go != gameObject)
+            {
+                float distance = Vector2.Distance(go.transform.position, this.transform.position);
+                if (distance <= spaceBetween)
+                {
+                    Vector2 direction = transform.position - go.transform.position;
+                    transform.Translate(direction * Time.fixedDeltaTime);
+                }
+            }
+        }
+    }
+}
